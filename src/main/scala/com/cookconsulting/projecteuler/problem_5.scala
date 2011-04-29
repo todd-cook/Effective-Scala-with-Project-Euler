bb059d0b8b0feee9f807eb87d6fe9ba4de91464a
todd-cook <todd.g.cook@gmail.com>
2011-04-28 22:47:36 -0700

    Initial working version

--- src/main/scala/com/cookconsulting/projecteuler/problem_5.scala
+++ src/main/scala/com/cookconsulting/projecteuler/problem_5.scala
@@ -30,7 +30,8 @@ package com.cookconsulting.projecteuler
 
 /**
 Problem 5
-2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+2520 is the smallest number that can be divided by each of the numbers
+from 1 to 10 without any remainder.
 What is the smallest number that is evenly divisible by all of the numbers from 1 to 20?
  *
  * @author : Todd Cook
